639261806d8a6e24048a91389f725fb94c304635
H4xScripts <pratikhamal.com@gmail.com>
2025-03-26 03:53:05 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var editor;
+
+require(["./vs/editor/editor.main"], ()  => {
+    editor = monaco.editor.create(document.getElementById("container"), {
+        value: "",
+        language: "lua",
+        theme: "vs-dark",
+        automaticlayout: true
+    });
+
+});
